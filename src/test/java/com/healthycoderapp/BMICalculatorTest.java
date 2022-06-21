c06a22006daa97a1847bba1da9275deec4c63c4f
MaciejKuchciak <kuchciak.maciej@gmail.com>
2022-06-21 23:40:23 +0200

    added additional test and enriched tests by given/when/then

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -7,7 +7,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class BMICalculatorTest {
 
     @Test
-    void test(){
-        assertTrue(BMICalculator.isDietRecommended(89.0,1.72));
+    void should_ReturnTrue_When_DietRecommended(){
+
+//      given
+        double weight = 89.0;
+        double height = 1.72;
+
+//      when
+        boolean recommended = BMICalculator.isDietRecommended(weight,height);
+
+//      then
+        assertTrue(recommended);
+    }
+
+    @Test
+    void should_ReturnFalse_When_DietNotRecommended(){
+
+//      given
+        double weight = 50.0;
+        double height = 1.92;
+
+//      when
+        boolean recommended = BMICalculator.isDietRecommended(weight,height);
+
+//      then
+        assertFalse(recommended);
     }
 }
