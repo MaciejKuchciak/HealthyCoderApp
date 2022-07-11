8974274ac44ac545da1650f04c31a2b470459772
MaciejKuchciak <kuchciak.maciej@gmail.com>
2022-07-11 23:05:42 +0200

    added parameterized test for diet recommendation

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,6 +29,21 @@ class BMICalculatorTest {
 
 //      given
         double weight = 89.0;
+        double height = 1.72;
+
+//      when
+        boolean recommended = BMICalculator.isDietRecommended(weight, height);
+
+//      then
+        assertTrue(recommended);
+    }
+
+    @ParameterizedTest
+    @ValueSource(doubles = {89.0, 95.0, 110.0})
+    void should_ReturnTrue_When_DietRecommended(Double coderWeight){
+
+//      given
+        double weight = coderWeight;
         double height = 1.72;
 
 //      when
