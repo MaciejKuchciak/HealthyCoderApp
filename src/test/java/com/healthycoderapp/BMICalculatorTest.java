4cfffcb3f07daa0c1aeb0f4d87c689d03c1f532f
MaciejKuchciak <kuchciak.maciej@gmail.com>
2022-07-11 22:16:04 +0200

    added null values assertion

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -72,4 +72,19 @@ class BMICalculatorTest {
 
     }
 
+    @Test
+    void should_ReturnNullWorstBMICoder_When_CoderListEmpty(){
+
+//        given
+        List<Coder> coders = new ArrayList<>();
+
+//        when
+        Coder coderWorstBMI = BMICalculator.findCoderWithWorstBMI(coders);
+
+//        then
+        assertNull(coderWorstBMI);
+
+
+    }
+
 }
