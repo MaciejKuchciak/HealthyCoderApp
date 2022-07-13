965f98a063399c9ef170aea12346e6929ca413b9
MaciejKuchciak <kuchciak.maciej@gmail.com>
2022-07-13 23:17:24 +0200

    added assumption to performance test

--- src/test/java/com/healthycoderapp/BMICalculatorTest.java
+++ src/test/java/com/healthycoderapp/BMICalculatorTest.java
@@ -6,16 +6,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.Executable;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 class BMICalculatorTest {
+
+    private String environment = "prod";
 
     @BeforeAll
     static void beforeAll(){
@@ -106,6 +107,7 @@ class BMICalculatorTest {
     @Test
     void should_ReturnCoderWithWorstBMIIn1Ms_When_CoderListHas10000Elements(){
 //      given
+        assumeTrue(this.environment.equals("prod"));
         List<Coder> coders = new ArrayList<>();
         for (int i=0; i < 10000; i++){
             coders.add(new Coder(1.0 + i, 10.0 +1));
